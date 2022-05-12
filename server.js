d261e922614384237c4efc228cf6d68b6030fba5
jevgenix <71660541+jevgenix@users.noreply.github.com>
2022-05-12 21:09:52 +0300

    heroku port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const uri = process.env.DB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
